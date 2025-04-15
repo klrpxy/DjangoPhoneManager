701ce5adbceb42ea8220c196162fa18e64c58072
Sun Qi Zhi <klrpxy@163.com>
2025-04-15 15:58:16 +0800

    asd

--- app_web/views.py
+++ app_web/views.py
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 from django.shortcuts import render
 
 # Create your views here.
+=======
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    return HttpResponse("login page")
+
+>>>>>>> e643b94 (add funcion login and bind in urls)
