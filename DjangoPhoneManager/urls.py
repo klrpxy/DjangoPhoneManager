46fe1ab413b435ee103ad45ebec31dbe893b6486
Sun Qi Zhi <klrpxy@163.com>
2025-04-15 16:33:05 +0800

    add funcion login and bind in urls

--- DjangoPhoneManager/urls.py
+++ DjangoPhoneManager/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-<<<<<<< HEAD
-# from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
-    #    path("admin/", admin.site.urls),
-=======
 from django.urls import path
 from app_web import views
 urlpatterns = [
     path("login/", views.login)
->>>>>>> e643b94 (add funcion login and bind in urls)
 ]
--- app_web/views.py
+++ app_web/views.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-=======
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -10,4 +5,4 @@ from django.shortcuts import render
 def login(request):
     return HttpResponse("login page")
 
->>>>>>> e643b94 (add funcion login and bind in urls)
+
