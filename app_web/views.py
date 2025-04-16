e62a0ebb05445fd5d0b73526e25cb90c2635f091
Sun Qi Zhi <klrpxy@163.com>
2025-04-16 14:03:15 +0800

    添加登入视图对用户名和密码的校验
    同时在数据库添加了用于测试的一行管理员数据
        name: wupeiqi
        pwd:  123456

--- app_web/views.py
+++ app_web/views.py
@@ -1,8 +1,8 @@
 from django import forms
 from captcha.fields import CaptchaField, CaptchaTextInput
 from django.shortcuts import render, redirect, HttpResponse
-
-
+from app_web import models
+from utils.encrypt import md5
 
 
 class CustomCaptchaTextInput(CaptchaTextInput):
@@ -11,7 +11,7 @@ class CustomCaptchaTextInput(CaptchaTextInput):
 
 class CaptchaTestForm(forms.Form):
     template_name = "form_snippet.html"
-    user_name = forms.CharField(
+    username = forms.CharField(
         label="用户名",
         widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "输入用户名"})
     )
@@ -34,6 +34,19 @@ def login(request):
     if not form.is_valid():
         form.add_error("captcha", "验证码输入错误")
         return render(request, 'login.html', {'form': form})
+
+    # 在数据库校验用户名和密码
+    input_name = form.cleaned_data["username"]
+    input_pwd = form.cleaned_data["password"]
+    encrypt_pwd = md5(input_pwd)
+    print(input_name, encrypt_pwd)
+    admin_object = models.Admin.objects.filter(username=input_name,password=encrypt_pwd).first()
+    if not admin_object:
+        form.add_error("username", "用户名或密码输入错误！")
+        return render(request, 'login.html', {'form': form})
+
+    request.session["info"] = {"id": admin_object.id, "name":admin_object.username}
+    request.session.set_expiry(60 * 60 * 24 * 7)
 
     return HttpResponse("验证成功")
 
